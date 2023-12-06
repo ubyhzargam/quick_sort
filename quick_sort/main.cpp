5c141b4bf1423590e39c07deba59c296a93afe73
ubyhzargam <ubyhzargam@gmail.com>
2023-12-06 22:49:28 +0530

    Initial Commit

---
+++ quick_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  quick_sort
+//
+//  Created by Uby H on 06/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
